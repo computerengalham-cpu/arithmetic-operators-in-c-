fd30b6a250d9a9e1f212ca51b4883bbe7169fc1d
Alham Saif <computerengalham@gmail.com>
2026-04-14 14:45:03 +0400

    Add files via upload
    arithmetic operators in c# language

---
+++ solution2/project1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("please Enter First number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please Enter Second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($" {num1}+ {num2}={num1+ num2}");
+            Console.WriteLine($" {num1}- {num2}={num1 - num2}");
+            Console.WriteLine($" {num1}* {num2}={num1 * num2}");
+            Console.WriteLine($" {num1}/ {num2}={num1 / num2}");
+
+
+
+        }
+    }
+}
